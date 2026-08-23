ca62e1d91a4e1f9d4779563b55f62da28d367171
Sanson <eduardobosanson@gmail.com>
2026-08-23 09:22:54 -0300

    fix: remove AuthProvider duplicado em main.tsx (App.tsx ja fornece o proprio)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './theme.css'
 import './index.css'
 import App from './App.tsx'
-import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </StrictMode>,
 )
